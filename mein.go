2061fb8cc41657b91b35723fa08da200595892a6
PolopTech <73751987+PolopTechnology@users.noreply.github.com>
2021-03-30 17:49:35 -0400

    Add files via upload

--- mein.go
+++ mein.go
@@ -9,21 +9,55 @@ import (
 func main() {
 	var i string
         fmt.Scanln(&i)
-        b := [4]string{"SELECT", "*", "FROM", "Table"}
-	syntax := strings.Split(i, ";")
-        var points int
-        for x := 0; x < len(syntax); x++ {
-            for j := x; j < x + 1; j++ {
-                if syntax[x] == b[j] {
-                   points += 1
-                }
+        var init int
+        split1 := strings.Split(i, ";")
+        for i := 0; i < len(split1); i++ {
+            if split1[i] == "SELECT" {
+               init = 1
+               break;
+            } else if split1[i] == "CREATE" {
+               init = 2
+               break;
             }
-        } 
-        if points >= 4 {
-           fmt.Println("id | name")
-        } else if points > 0 && points < 4 {
-           fmt.Println("SYNTAX ERROR! INCOMPLETE SENTENCE!")
         }
+        
+        file := `1{Table
+1}1[1d;id, 2d;name
+--1}1d]1-2
+--1}2d]Sergio-Ana`
+        
+        if init == 1 {
+           split2 := strings.Split(file, "{")
+           for a := 0; a < len(split2); a++ {
+               split3 := strings.Split(split2[a], "[")
+               if strings.Contains(split3[0], split1[3]) == true {
+                  if split1[1] == "*" {
+                     fmt.Println(split3[1])
+                     break;
+                  } else {
+                     var bk bool = false
+                     split4 := strings.Split(split3[1], "--")
+                     s5 := strings.Split(split4[0], ",")
+                     for b := 0; b < len(s5); b++ {
+                         if strings.Contains(s5[b], split1[1]) {
+                            s6 := strings.Split(s5[b], ";")
+                            for c := 1; c < len(split4); c++ {
+                                six := strings.TrimSpace(s6[0])
+                                if strings.Contains(split4[c], six) == true {
+                                   var1 := strings.Split(split4[c], "]")
+                                   fmt.Println(var1[1])
+                                   bk = true
+                                }
+                            }
+                         }
+                         if bk == true {
+                            break;
+                         }
+                     }
+                  }
+               }
+           }
+        } 
 }
 
 
